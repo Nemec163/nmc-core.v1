3346ff3350c97177399539888b68cedc54d75528
Nemec163 <avnemkov@inbox.ru>
2025-04-16 03:17:02 +0400

    test

--- packages/nmc-ui.v1/.storybook/main.ts
+++ packages/nmc-ui.v1/.storybook/main.ts
@@ -1,31 +1,33 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
+import { join, dirname } from "path"
+
+/**
+* This function is used to resolve the absolute path of a package.
+* It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-interactions')
+  ],
   framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
+    name: getAbsolutePath('@storybook/react-webpack5'),
+    options: {}
   },
-  webpackFinal: async (config) => {
-    config.module?.rules?.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [
-              require.resolve('@babel/preset-env'),
-              require.resolve('@babel/preset-react'),
-              require.resolve('@babel/preset-typescript'),
-            ],
-          },
-        },
-      ],
-    });
-    config.resolve!.extensions!.push('.ts', '.tsx');
-    return config;
-  },
+  managerHead: (head) => `
+    ${head}
+    <base href="/ui/" />
+  `,
 };
 
 export default config;
